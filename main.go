a00ddc3f41f8551d41f0a6eecc7a6c180455fa57
Raymond Perminov <raymond@brizoit.com>
2024-06-21 19:12:12 +0300

    Added dynamic icon

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"math"
 	"os"
 	"time"
 
+	"github.com/fogleman/gg"
 	"github.com/getlantern/systray"
 )
 
 var (
 	timezone string
+
+	second int
 )
 
 func main() {
@@ -33,6 +42,11 @@ func onReady() {
 		for {
 			systray.SetTitle(getClockTime(timezone))
 			systray.SetTooltip(timezone + " timezone")
+			systray.SetIcon(createProgressIcon(float64(second) / float64(60)))
+			second++
+			if second == 60 {
+				second = 0
+			}
 			time.Sleep(1 * time.Second)
 		}
 	}()
@@ -76,4 +90,40 @@ func getIcon(filePath string) []byte {
 		fmt.Printf("Error during downloading icon: %v\n", err)
 	}
 	return icon
+}
+
+// createProgressIcon creates an icon with a progress circle
+func createProgressIcon(progress float64) []byte {
+	const size = 64
+	const borderThickness = 4 // Thickness of the border
+	const radiusOffset = 4    // Offset to reduce the radius
+	dc := gg.NewContext(size, size)
+
+	// Draw transparent background
+	dc.SetColor(color.RGBA{0, 0, 0, 0}) // Transparent color
+	dc.Clear()
+
+	// Draw progress circle
+	dc.SetColor(color.RGBA{0, 255, 0, 255}) // Green color
+	startAngle := -gg.Radians(90)
+	endAngle := startAngle + (2 * math.Pi * progress)
+	radius := float64(size)/2 - radiusOffset
+	dc.DrawArc(float64(size)/2, float64(size)/2, radius, startAngle, endAngle)
+	dc.LineTo(float64(size)/2, float64(size)/2)
+	dc.ClosePath()
+	dc.Fill()
+
+	// Draw outer border
+	dc.SetLineWidth(borderThickness)
+	dc.SetColor(color.RGBA{0, 255, 0, 255}) // Green color
+	dc.DrawCircle(float64(size)/2, float64(size)/2, radius)
+	dc.Stroke()
+
+	// Save to buffer
+	img := image.NewRGBA(image.Rect(0, 0, size, size))
+	draw.Draw(img, img.Bounds(), dc.Image(), image.Point{0, 0}, draw.Src)
+
+	buf := new(bytes.Buffer)
+	png.Encode(buf, img)
+	return buf.Bytes()
 }
